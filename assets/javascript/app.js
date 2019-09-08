996fa048129ce68f9ebb92579ee34cd5b06f6805
Michelle M <53539385+michelle-88@users.noreply.github.com>
2019-09-08 16:24:46 -0400

    created an array of 10 question and answer objects.

---
+++ assets/javascript/app.js
@@ -0,0 +1,42 @@
+// Declare global variables
+var triviaQuestions = [
+    { q: "What was the name of Mulan's dragon companion?",
+      choices: ["Pascal", "Toothless", "Mushu", "Puff"],
+      a: 3 },
+    
+    { q: "What was Ariel's term for a fork in The Little Mermaid?",
+      choices: ["Whatchamacallit", "Dinglehopper", "Whosit", "Whatsit"],
+      a: 2 },
+
+    { q: "Frollo is the villian in which Disney movie?",
+      choices: ["Hercules", "The Lion King", "The Sword in the Stone", "The Hunchback of Notre Dame"],
+      a: 4 },
+
+    { q: "What was the name of Jasmine's pet tiger in Aladdin?",
+      choices: ["Tony", "Rajah", "Tina", "Abu"],
+      a: 2 },
+
+    { q: "Finish this lyric from The Lion King: 'Oh, I just can't wait to be ...'",
+      choices: ["free", "famous", "king", "queen"],
+      a: 3 },
+
+    { q: "What is the name of Woody and Buzz's owner in Toy Story?",
+      choices: ["Andy", "Bob", "Molly", "Fred"],
+      a: 1 },
+
+    { q: "In Finding Nemo, which city do Marlin and Dory travel to in order to find Nemo?",
+      choices: ["San Diego", "Miami", "Sydney", "Melbourne"],
+      a: 3 },
+    
+    { q: "What is the name of the megacorporation that manufactured the robot Wall-E?",
+      choices: ["Walmart", "Buy-N-Large", "ACME Corp", "Amazon"],
+      a: 2 },
+
+    { q: "What is the name of Riley's imaginary friend in the movie Inside Out?",
+      choices: ["Bing Bong", "Waldo", "Forky", "Giggles"],
+      a: 1},
+
+    { q: "What is the name of the city where Mike and Sully live in Monsters Inc?",
+      choices: ["Metroville", "Scareville", "Monstropolis", "San Fransokyo"],
+      a: 3 }
+];
