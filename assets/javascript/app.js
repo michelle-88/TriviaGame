2b3a6050b39a7f750be84ec9be224562aa410e20
Michelle M <53539385+michelle-88@users.noreply.github.com>
2019-09-09 21:33:08 -0400

    modified answer properties of each question object. changed them from index number to actual answer string to simplify code. created loadNextQuestion function that will run the necessary code to show next question without user input. this function is then called via a setTimeout after player chooses answer or runs out of time. removed function declaration for answerClick function from within p tag click listener. declared this function globally instead so that it could be called later.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,43 +4,43 @@ $(document).ready(function () {
 var triviaQuestions = [
     { q: "What was the name of Mulan's dragon companion?",
       choices: ["Pascal", "Toothless", "Mushu", "Puff"],
-      a: 2 },
+      a: "Mushu" },
     
     { q: "What was Ariel's term for a fork in The Little Mermaid?",
       choices: ["Whatchamacallit", "Dinglehopper", "Whosit", "Whatsit"],
-      a: 1 },
+      a: "Dinglehopper" },
 
     { q: "Frollo is the villian in which Disney movie?",
       choices: ["Hercules", "The Lion King", "The Sword in the Stone", "The Hunchback of Notre Dame"],
-      a: 3 },
+      a: "The Hunchback of Notre Dame" },
 
     { q: "What was the name of Jasmine's pet tiger in Aladdin?",
       choices: ["Tony", "Rajah", "Tina", "Abu"],
-      a: 1 },
+      a: "Rajah" },
 
     { q: "Finish this lyric from The Lion King: 'Oh, I just can't wait to be ...'",
       choices: ["free", "famous", "king", "queen"],
-      a: 2 },
+      a: "king" },
 
     { q: "What is the name of Woody and Buzz's owner in Toy Story?",
       choices: ["Andy", "Bob", "Molly", "Fred"],
-      a: 0 },
+      a: "Andy" },
 
     { q: "In Finding Nemo, which city do Marlin and Dory travel to in order to find Nemo?",
       choices: ["San Diego", "Miami", "Sydney", "Melbourne"],
-      a: 2 },
+      a: "Sydney" },
     
     { q: "What is the name of the megacorporation that manufactured the robot Wall-E?",
       choices: ["Walmart", "Buy-N-Large", "ACME Corp", "Amazon"],
-      a: 1 },
+      a: "Buy-N-Large" },
 
     { q: "What is the name of Riley's imaginary friend in the movie Inside Out?",
       choices: ["Bing Bong", "Waldo", "Forky", "Giggles"],
-      a: 0},
+      a: "Bing Bong" },
 
     { q: "What is the name of the city where Mike and Sully live in Monsters Inc?",
       choices: ["Metroville", "Scareville", "Monstropolis", "San Fransokyo"],
-      a: 2 }
+      a: "Monstropolis" }
 ];
 
 var numCorrect = 0;
@@ -53,16 +53,16 @@ var timerText = document.querySelector("#timer");
 var timerInt;
 
 
-// Write function that will show each question based on current questionIndex
+// Write function that will show each question based on current questionIndex and start countdown
 function showQuestion(){
-    $("#question").empty();
+    timerInt = setInterval(timerCountdown, 1000);
     $("#question").text(triviaQuestions[questionIndex].q);
 }
 
 // Write function that will show answer choices for the current question
 function showChoices(){
     for(var i = 0; i < 4; i++){
-        $("#answerChoices").append("<p choice =" + i + ">" + triviaQuestions[questionIndex].choices[i] + "</p>");
+      $("#answerChoices").append("<p>" + triviaQuestions[questionIndex].choices[i] + "</p>");
     }
 }
 
@@ -70,11 +70,12 @@ function showChoices(){
 function timerCountdown(){
     if(timeLeft == 0){
         clearInterval(timerInt);
-        $("#question").hide();
-        $("#answerChoices").hide();
-        $("#displayText").html("<h3>Time's Up!</h3> <p>The Correct Answer was: " + triviaQuestions[questionIndex].choices[triviaQuestions[questionIndex].a] + "</p>");
+        $("#question").empty();
+        $("#answerChoices").empty();
+        $("#displayText").html("<h3>Time's Up!</h3> <p>The Correct Answer was: " + triviaQuestions[questionIndex].a + "</p>");
         numUnanswered++;
         questionIndex++;
+        setTimeout(loadNextQuestion, 5000);
     }
 
     else {
@@ -83,14 +84,48 @@ function timerCountdown(){
     }
 }
 
+// Write function that will run when user clicks an answer choice. User's choice will be stored and compared to correct answer.
+function answerClick(event){
+
+  userChoice = event.target.textContent;
+  
+  if(userChoice == triviaQuestions[questionIndex].a){
+      $("#question").empty();
+      $("#answerChoices").empty();
+      $("#displayText").html("<h3>Correct!</h3>")
+      clearInterval(timerInt);
+      numCorrect++;
+      questionIndex++;
+      setTimeout(loadNextQuestion, 5000);
+  }
+
+  else {
+      $("#question").empty();
+      $("#answerChoices").empty();
+      clearInterval(timerInt);
+      $("#displayText").html("<h3>Wrong!</h3> <p>The Correct Answer was: " + triviaQuestions[questionIndex].a + "</p>");
+      numIncorrect++;
+      questionIndex++;
+      setTimeout(loadNextQuestion, 5000);
+  }
+  
+}
+
+// Write function to show next question, answer choices, and reset timer
+function loadNextQuestion(){
+  $("#displayText").empty();
+  timeLeft = 30;
+  timerText.textContent = timeLeft;
+  showQuestion();
+  showChoices();
+  $("p").on("click", answerClick);
+}
+
 // Hide question/answers area at start of game
 $("#triviaArea").hide();
 
-// Click listener to hide button/start game when start button is clicked
+// Click listener for start button that will hide button & start game when button is clicked
 $("#startBtn").on("click", function(){
-    
-    timerInt = setInterval(timerCountdown, 1000);
-    
     $(this).hide();
     $("#triviaArea").show();
 
@@ -98,35 +133,12 @@ $("#startBtn").on("click", function(){
     showQuestion();
     showChoices();
 
-    // Click listener for answer choices - store user's answer and compare to correct answer
-    $("p").on("click", function(event){
-        userChoice = event.target.getAttribute("choice");
-        
-        if(userChoice == triviaQuestions[questionIndex].a){
-            $("#question").hide();
-            $("#answerChoices").hide();
-            $("#displayText").html("<h3>Correct!</h3>")
-            clearInterval(timerInt);
-            numCorrect++;
-            questionIndex++;
-        }
+    // Click listener for answer choices
+    $("p").on("click", answerClick);
 
-        else {
-            $("#question").hide();
-            $("#answerChoices").hide();
-            clearInterval(timerInt);
-            $("#displayText").html("<h3>Wrong!</h3> <p>The Correct Answer was: " + triviaQuestions[questionIndex].choices[triviaQuestions[questionIndex].a] + "</p>");
-            numIncorrect++;
-            questionIndex++;
-        }
-        
-
-    });
-
-   
+  
 });
-// Write function for 5 second countdown for results page after each question
-    // once this countdown finishes, next question will be displayed
+
 // Write logic to display # correct, wrong, unanswered at end of game
 // Write function to restart game when player clicks 'Start Over' button
 
